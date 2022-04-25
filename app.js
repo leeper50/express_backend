b52ac5e6d875c644d4eae9f97f5be73c911772c4
Walter Leeper <wleeper50@hotmail.com>
2022-04-25 11:20:44 -0500

    next commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ con.connect( function(err) {
     console.log("Database connection established!");
 });
 
-
+let port = 9000
 const app = express();
 const server = http.createServer(app);
 
@@ -27,68 +27,71 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'./public')));
 
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
-
+/**
+ * This function will take a post request with email,username, and password fields
+ * After successful registration, the user may register another, or move to the login page
+ * Failure will result in a re-registration occuring
+ */
 app.post('/register', async (req, res) => {
-    // TODO: check if email or name is in use already
+    console.log("register called")
+    let exist_query = `SELECT 1 FROM user_information WHERE user_name = '${req.body.username}'`;
     let user_exists = false;
-    let exist_query = `SELECT 1 FROM user_information WHERE user_email = '${req.body.email}'`;// OR user_name = '${req.body.user_name}'`;
     await con.query(exist_query, function(err, result) {
-        //console.table(result)
-        console.log("result[0]", result[0])
-        console.log("result[0][1]", result[0][1])
         if (err) throw err;
-        if (result[0][1] != 0)
+        if (typeof result[0] != 'undefined')
             user_exists = true;
-    });
-    console.log(user_exists)
-    
-    try {
-        if (user_exists == false) {    
-            let hashPassword = await bcrypt.hash(req.body.password, 10);
-            let newUser = {
-                username: req.body.username,
-                email: req.body.email,
-                password: hashPassword,
-            };
 
-            let query = `INSERT INTO user_information (user_name,user_password,user_email,user_code) ` +
-            `VALUES ('${newUser.username}','${newUser.password}','${newUser.email}',left(uuid(), 8));`;
-
-            con.query(query, function(err, result) {
-                if (err) throw err;
-                console.log("1 record inserted");
-            });
+        try {
+            if (user_exists == false) {
+                let hashPassword = bcrypt.hashSync(req.body.password, 10);
+                let newUser = {
+                    username: req.body.username,
+                    password: hashPassword,
+                };
     
-            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
-        } else {
-            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+                let query = `INSERT INTO user_information (user_name,user_password,user_email,user_code) ` +
+                `VALUES ('${newUser.username}','${newUser.password}','default@example.com',left(uuid(), 8));`;
+
+                con.query(query, function(err, result) {
+                    if (err) throw err;
+                    console.log(`${newUser.username} has been added.`);
+                });
+                res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+            } else {
+                res.send("<div align ='center'><h2>User already exists</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+            }
+        } catch{
+            res.send("Internal server error");
         }
-    } catch{
-        res.send("Internal server error");
-    }
-    console.log("at end",user_exists)
+    });
 });
 
+/**
+ * This function will take a post request with email and password fields
+ * After successful authentication, the user will move to their home page
+ * Failure will result in a relogin initiated
+ */
 app.post('/login', async (req, res) => {
-    // TODO: fix incorrect user name crashing the server
     try {
         let storedPass;
-        let query = `SELECT * FROM user_information WHERE user_email = '${req.body.email}'`;
-        con.query(query, function(err, result) {
-            if (result[0] == undefined)
-                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
-            else {
-                storedPass = setPass(result[0].user_password);
+        let query = `SELECT * FROM user_information WHERE user_name = '${req.body.username}'`;
+        await con.query(query, function(err, result) {
+            if (result[0] == undefined) {
+                res.redirect('/')
+                //res.send("<div align ='center'><h2>Invalid name or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+            } else {
+                storedPass = result[0].user_password;
                 const passwordMatch = bcrypt.compareSync(req.body.password, storedPass);
                 if (passwordMatch) {
                     let username = result[0].user_name;
-                    res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${username}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+                    res.redirect('/game');
+                    //res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${username}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
                 } else {
-                    res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+                    res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='/'>login again</a></div>");
                 }
             }
         });
@@ -97,10 +100,31 @@ app.post('/login', async (req, res) => {
     }
 });
 
-function setPass(result) {
-    return result;
-}
+/**
+ * This function will allow the user to load their game's state
+ * The state will be returned as a json object
+ */
+let shared_obj = {}
+app.get('/state', async(req, res) => {
+    let testobj = {
+        name: "name1",
+        id: 100,
+        gpa: 4.0
+    };
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(testobj));
+    shared_obj = testobj;
+    //res.json(JSON.stringify(testobj));
+});
 
-server.listen(3000, function(){
-    console.log("server is listening on port: 3000");
+/**
+ * This function will allow the user to save their game's state
+ * The state should be sent as a json object
+ */
+app.post('/state', async(req, res) => {
+    res.send(shared_obj)
+});
+
+server.listen(port, function(){
+    console.log(`Server is listening on port: ${port}`);
 });
